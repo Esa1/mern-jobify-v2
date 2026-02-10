ea8c478613a8641156d51009674421d6351eaa4a
Esa Torkkila <esa.torkkila@gmail.com>
2026-02-10 18:09:11 +0200

    Finish Landing page and add components folder

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 /* const StyledButton = styled.button`
   font-size: 1.5rem;
@@ -10,20 +15,31 @@ import styled from "styled-components";
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing</h1>;<div className="content">some content</div>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+            quaerat tempore rerum quibusdam possimus molestiae placeat vel fuga
+            numquam maxime architecto, aperiam incidunt porro minima vero
+            laborum, quidem praesentium recusandae!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  background: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue;
-    color: yellow;
-  }
-`;
 
 export default Landing;
