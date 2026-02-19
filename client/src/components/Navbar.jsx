e6b1b1c9d6dc01d788b4893031a821b4e01577e8
Esa Torkkila <esa.torkkila@gmail.com>
2026-02-19 15:25:55 +0200

    Links data

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
-// import { FaHome } from "react-icons/fa";
 import { FaAlignLeft } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDashboardContext } from "../pages/DashboardLayout";
-const { toggleSidebar } = useDashboardContext();
 
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
       <div className="nav-center">
